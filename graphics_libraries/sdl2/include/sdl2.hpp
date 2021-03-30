2f1b83a641661908ec95472bb50a2305823d105e
Alexis THOMAS <alexis.thomas@epitech.eu>
2021-03-30 15:46:19 +0200

    [ADD] sdl2.hpp

--- graphics_libraries/sdl2/include/sdl2.hpp
+++ graphics_libraries/sdl2/include/sdl2.hpp
@@ -8,6 +8,35 @@
 #ifndef SLD2_HPP_
 #define SLD2_HPP_
 
-int setup();
+#include "IGraphicLib.hpp"
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_timer.h>
+#include <locale.h>
+
+class sdl2Lib : public IGraphicLib
+{
+public:
+    virtual void init_lib();
+    virtual void exit_lib();
+    virtual int keyPressed() const;
+    virtual void printMap(std::vector<std::string>);
+    virtual void printButton(int x, int y, std::string);
+    virtual void printSelectedButton(int x, int y, std::string);
+    virtual void printText(int x, int y, std::string);
+    virtual void clearWindow();
+    virtual void refreshWindow();
+    virtual void printWindow();
+    virtual void printTitle(std::string);
+    virtual std::pair<int, int> getWindowSize() const;
+    virtual void assetLoader(const std::string);
+protected:
+private:
+    SDL_Window* win;
+    SDL_Renderer *renderer;
+    std::map<std::string, std::vector<std::string>> _maps;
+};
 
 #endif /* !SLD2_HPP_ */
